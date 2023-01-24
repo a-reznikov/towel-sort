f024e196ddd1738f40b26e53e2132c2fe23c755b
a-reznikov <a.reznnikov@gmail.com>
2023-01-24 18:11:39 +0300

    feat: implement solushion

--- src/index.js
+++ src/index.js
@@ -2,5 +2,28 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  console.log(matrix);
+  let newArray = [];
+  let sortArray = [];
+  for (arrays in matrix) {
+    console.log(arrays);
+    if (arrays % 2 === 0) {
+      sortArray.push(matrix[arrays]);
+      // console.log(arrays, ":", matrix[arrays])
+    } else {
+      sortArray.push(matrix[arrays].sort((a, b) => b - a));
+    }
+    // console.log(arrays);
+    // matrix[arrays].forEach(element => {
+    //   newArray.push(element);
+    // });
+  }
+  for (arrays in sortArray) {
+    sortArray[arrays].forEach(element => {
+    newArray.push(element);
+    });
+  }
+  // console.log(sortArray);
+  // console.log(newArray);
+  return newArray;
 }
